6f2072fd852c524d586af6a8a79876028ae6d95a
MiKarpov <mikhailkarpov@hotmail.com>
2020-06-17 13:53:47 +0300

    Add sorting reports

--- src/main/java/com/mikhailkarpov/coronavirustracker/controller/HomeController.java
+++ src/main/java/com/mikhailkarpov/coronavirustracker/controller/HomeController.java
@@ -6,16 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
 
+    private static final String SORT_BY_COUNTRY = "country";
+    private static final String SORT_BY_CONFIRMED = "confirmed";
+    private static final String SORT_BY_RECOVERED = "recovered";
+    private static final String SORT_BY_DEATHS = "deaths";
+    private static final String ORDER_ASCENDING = "asc";
+    private static final String ORDER_DESCENDING = "desc";
+
     private final ReportsService service;
     private final Comparator<Report> comparatorByConfirmed = Comparator.comparingInt(Report::getConfirmed).reversed();
+
 
     @Autowired
     public HomeController(ReportsService service) {
@@ -23,10 +33,13 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String homePage(Model model) {
-        List<Report> lastReports = service.getLastReports()
+    public String homePage(@RequestParam Optional<String> sortBy, Model model) {
+        Comparator<Report> comparator = getComparator(sortBy);
+
+        List<Report> lastReports = service
+                .getLastReports()
                 .stream()
-                .sorted(comparatorByConfirmed)
+                .sorted(comparator)
                 .collect(Collectors.toList());
 
         int totalConfirmed = lastReports.stream().mapToInt(Report::getConfirmed).sum();
@@ -40,5 +53,26 @@ public class HomeController {
         model.addAttribute("lastUpdate", service.getLastUpdate());
 
         return "latestReport";
+    }
+
+    private Comparator<Report> getComparator(Optional<String> sortBy) {
+        Comparator<Report> comparator = Comparator.comparing(Report::getCountry);
+
+        if (!sortBy.isPresent())
+            return comparator;
+
+        switch (sortBy.get()) {
+            case (SORT_BY_CONFIRMED):
+                comparator = Comparator.comparing(Report::getConfirmed).reversed();
+                break;
+            case (SORT_BY_RECOVERED):
+                comparator = Comparator.comparing(Report::getRecovered).reversed();
+                break;
+            case (SORT_BY_DEATHS):
+                comparator = Comparator.comparing(Report::getDeaths).reversed();
+                break;
+        }
+
+        return comparator;
     }
 }
